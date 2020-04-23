fb77091e191963c1947c41a5beed7f7c3da3bed7
Marieke Jackson <marieke.jackson@gmail.com>
2020-04-23 15:03:07 -0400

    still broken but moved pipelines to one repos.py dict

--- pipelines/dagster_examples/repos.py
+++ pipelines/dagster_examples/repos.py
@@ -9,16 +9,18 @@ sys.path.append(script_relative_path('.'))
 
 from hello_cereal import hello_cereal_pipeline
 from complex_pipeline import complex_pipeline
+from xgboost_test import demo_pipeline
 
 
 def define_repo():
     return RepositoryDefinition(
-        name='hello_cereal_repository',
+        name='test_repo',
         # Note that we can pass a function, rather than pipeline instance.
         # This allows us to construct pipelines lazily, if, e.g.,
         # initializing a pipeline involves any heavy compute
         pipeline_dict={
             'hello_cereal_pipeline': lambda: hello_cereal_pipeline,
             'complex_pipeline': lambda: complex_pipeline,
+            'xgboost_demo': lambda: demo_pipeline,
         },
     )
